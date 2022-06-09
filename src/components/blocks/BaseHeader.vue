08bad2ac1861331ade6f54525627f5e4ec514343
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-09 12:39:35 +0200

    scafholding

---
+++ src/components/blocks/BaseHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+     <img src="../../assets/img/netflix-false.png" alt="">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'BaseHeader'
+}
+</script>
+
+<style lang="scss" scoped> 
+  header{
+     height: 6.25rem;
+     background-color: black;
+     img{
+        width: 7.125rem;
+        margin-top:.3125rem ;
+     }
+  }
+
+</style>
