bcc76ec0d3dfd4d9aecd00d559f148ac8ad95b42
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-09 21:03:09 +0200

    img inclusa nel ciclo

--- src/components/section/sectionFilm.vue
+++ src/components/section/sectionFilm.vue
@@ -1,6 +1,7 @@
 <template>
   <ul>
      <li v-for="film in sharedComponents.films" :key="film.id">
+          <img :src="'http://image.tmdb.org/t/p/w342' + film.poster_path" alt="">
           <h3>{{film.title}}</h3>  
           <h4>{{film.original_title}}</h4> 
           <!-- <img :src="getFlegs(film.original_language)" alt=""> -->
@@ -15,6 +16,10 @@
 import sharedComponents from '../shared/sharedComponents'
 export default {
  name: 'sectionFilm',
+ props:{
+   films: Array,
+   Tv: Array,
+ },
  data(){
     return{
        sharedComponents,
@@ -24,12 +29,12 @@ export default {
  methods:{
     getFlegs(language){
        return language === 'en' ? 'gb' : language === 'ja' ? 'jp' : language;
+    },
+    
     }
- },
- created(){
-     this.getFlegs(sharedComponents.films)
  }
-}
+ 
+
 </script>
 
 <style>
